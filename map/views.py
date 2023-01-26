e743b65f7d34d9364a1a20a0ae4639b1f3947c4a
mnibor <marcelorobin@gmail.com>
2023-01-26 19:50:37 -0300

    culstering agregado

--- map/views.py
+++ map/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Location
 import folium
+from folium.plugins import FastMarkerCluster
 
 def home(request):
     # Recupero todas las sucursales
@@ -9,9 +10,21 @@ def home(request):
     # Defino el mapa
     initialMap = folium.Map(location=[-34.6460675,-58.5268284], zoom_start=11)
 
-    for location in locations:
-        coordinates = (location.lat, location.lng)
-        folium.Marker(coordinates, popup='Sucursal '+ location.name).add_to(initialMap)
+    # Creamos el Clustering de los marcadores
+    latitudes = [location.lat for location in locations]
+    longitudes = [location.lng for location in locations]
+    popups = [location.name for location in locations]
+
+    print(latitudes)
+    print(longitudes)
+    print(list(zip(latitudes, longitudes, popups)))
+
+    FastMarkerCluster(data=list(zip(latitudes, longitudes, popups))).add_to(initialMap)
+
+    # Creamos los marcadores
+    # for location in locations:
+    #     coordinates = (location.lat, location.lng)
+    #     folium.Marker(coordinates, popup='Sucursal ' + location.name).add_to(initialMap)
 
     context = {'map':initialMap._repr_html_(), 'locations':locations}
     return render(request, 'map/home.html', context)
